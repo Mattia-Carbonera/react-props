1f11045a891241df4af25d0338e2f58cc63379a8
Mattia-Carbonera <mattia.carbonera@gmail.com>
2024-11-29 15:24:04 +0100

    add posts

--- src/components/card/card.jsx
+++ src/components/card/card.jsx
@@ -2,20 +2,16 @@ import style from "./card.module.css";
 import Button from "../button/button";
 import defaultImage from "../../assets/img/card-image.png";
 
-function Card() {
+function Card({ id, title, image, content, tags, published }) {
   return (
-    <div className={style.card}>
+    <div className={style.card} key={id}>
       <div className="image-container">
         <img src={defaultImage} alt="card image" className={style.img} />
       </div>
       <div className={style.description}>
-        <h2>Titolo del post</h2>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore optio
-          quibusdam facilis, quaerat accusamus molestiae repellendus odio error
-          expedita. Soluta dignissimos hic optio voluptas culpa molestias, est
-          facere veritatis veniam!
-        </p>
+        <h2>{title}</h2>
+        <p>{content}</p>
+        <span>{tags}</span>
         <div className="button">
           <Button />
         </div>
--- src/components/main/main.jsx
+++ src/components/main/main.jsx
@@ -1,10 +1,24 @@
 import Card from "../card/card";
+import posts from "../../db/posts";
 import defaultImage from "../../assets/img/card-image.png";
 
 function Main() {
+  const filteredPosts = posts.filter((post) => post.published == true);
+
   return (
     <main>
-      <Card />
+      {filteredPosts.map((post) => {
+        return (
+          <Card
+            key={post.id}
+            title={post.title}
+            image={post.image}
+            content={post.content}
+            tags={post.tags}
+            published={post.published}
+          />
+        );
+      })}
     </main>
   );
 }
