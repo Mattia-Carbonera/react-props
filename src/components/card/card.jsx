524263b785435c951b52a12ea8306fc88d888b37
Mattia-Carbonera <mattia.carbonera@gmail.com>
2024-11-29 16:02:56 +0100

    add code

--- src/components/card/card.jsx
+++ src/components/card/card.jsx
@@ -6,12 +6,20 @@ function Card({ id, title, image, content, tags, published }) {
   return (
     <div className={style.card} key={id}>
       <div className="image-container">
-        <img src={defaultImage} alt="card image" className={style.img} />
+        <img
+          src={image || defaultImage}
+          alt="card image"
+          className={style.img}
+        />
       </div>
       <div className={style.description}>
         <h2>{title}</h2>
         <p>{content}</p>
-        <span>{tags}</span>
+        {/* <span>{tags}</span> */}
+        {tags.forEach((tag) => {
+          console.log(tag);
+          return <span>{tag}</span>;
+        })}
         <div className="button">
           <Button />
         </div>
